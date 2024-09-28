2e9ac5cc891e2312a1d284c224e239d9b3666c53
berni <bod.kato@gmail.com>
2024-09-28 19:25:08 +0200

    initial commit

---
+++ configuration.nix
@@ -0,0 +1,51 @@
+{ pkgs, config, lib, inputs, ... }:
+{
+  services.openssh = {
+    enable = true;
+    settings.PermitRootLogin = "yes";
+  };
+
+  networking.hostName = "pi";
+
+  users.users.root.openssh.authorizedKeys.keys = [
+  "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH8diTl0La1Yyv4OwSZBpnZrESv6edKsNze1Z88u4U5a bod.kato@gmail.com"
+  ];
+
+  # users.users.berni = {
+  #       isNormalUser = true;
+  #       extraGroups = [ "wheel" ];
+  #       openssh.authorizedKeys.keys = [
+  #           # This is my public key
+  #           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH8diTl0La1Yyv4OwSZBpnZrESv6edKsNze1Z88u4U5a bod.kato@gmail.com"
+  #         ];
+  # };
+
+  environment.systemPackages = with pkgs; [
+      vim
+      curl
+      wget
+      inputs.helix.packages."${pkgs.system}".helix
+      git
+    ];
+
+  # "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix" creates a
+  # disk with this label on first boot. Therefore, we need to keep it. It is the
+  # only information from the installer image that we need to keep persistent
+  fileSystems."/" =
+    { device = "/dev/disk/by-label/NIXOS_SD";
+      fsType = "ext4";
+    };
+  boot = {
+    kernelPackages = lib.mkForce pkgs.linuxPackages_latest;
+    loader = {
+      generic-extlinux-compatible.enable = lib.mkDefault true;
+      grub.enable = lib.mkDefault false;
+    };
+  };
+  nix.settings = {
+    experimental-features = lib.mkDefault "nix-command flakes";
+    trusted-users = [ "root" "@wheel" ];
+  };
+
+  system.stateVersion = "24.11";
+}
---
+++ flake.nix
@@ -0,0 +1,67 @@
+{
+  description = "My raspberry pi confix";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    nixos-hardware.url = "github:nixos/nixos-hardware";
+    helix.url = "github:helix-editor/helix";
+    deploy-rs.url = "github:serokell/deploy-rs";
+    lix-module = {
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.91.0.tar.gz";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    inputs@{
+      self,
+      nixpkgs,
+      nixos-hardware,
+      lix-module,
+      deploy-rs,
+      ...
+    }:
+    {
+      nixosConfigurations = {
+        pi = nixpkgs.lib.nixosSystem {
+          system = "aarch64-linux";
+          specialArgs = { inherit inputs; };
+          modules = [
+            nixos-hardware.nixosModules.raspberry-pi-4
+            "${nixpkgs}/nixos/modules/profiles/minimal.nix"
+            lix-module.nixosModules.default
+
+            ./configuration.nix
+          ];
+        };
+      };
+
+      # sd card image
+      images = {
+        pi =
+          (self.nixosConfigurations.pi.extendModules {
+            modules = [
+              "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
+              {
+                disabledModules = [ "profiles/base.nix" ];
+                sdImage.compressImage = false;
+              }
+            ];
+          }).config.system.build.sdImage;
+      };
+
+      # deploy-rs configuration
+      deploy.nodes.berni-pi = {
+        hostname = "Berni-Pi";
+        remoteBuild = true;
+        profiles.system = {
+          user = "root";
+          sshUser = "root";
+          path = deploy-rs.lib.aarch64-linux.activate.nixos self.nixosConfigurations.pi;
+        };
+      };
+      checks = builtins.mapAttrs (system: deployLib: deployLib.deployChecks self.deploy) deploy-rs.lib;
+
+    };
+}
