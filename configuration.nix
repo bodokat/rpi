e74567606d4a6bf3b9e85ba1b21c6589e73ce147
berni <bod.kato@gmail.com>
2024-10-01 18:56:43 +0200

    some changes

--- configuration.nix
+++ configuration.nix
@@ -26,6 +26,63 @@
     git
   ];
 
+  services.deluge = {
+    enable = true;
+    web.enable = true;
+
+    openFirewall = true;
+  };
+
+  services.jackett = {
+    enable = true;
+
+  };
+
+  services.radarr = {
+    enable = true;
+  };
+
+  services.jellyfin = {
+    enable = true;
+  };
+
+  services.jellyseerr = {
+    enable = true;
+  };
+
+  networking.wg-quick.interfaces.protonvpn = {
+    autostart = true;
+    address = [ "10.2.0.2" ];
+    listenPort = 32;
+    privateKey = "mFkBhnjSlGoqGMVQ+r+ur51aKVQ8/wYjpymPtRYPAEE=";
+
+    peers = [
+      {
+        publicKey = "+kfPCjoNEateo3jVc9tcduKh6nwQpoKx0/JXxgjHD2c=";
+        allowedIPs = [
+          "0.0.0.0/0"
+          "::/0"
+        ];
+        endpoint = "190.2.147.7:51820";
+      }
+    ];
+  };
+
+  #   [Interface]
+  # # Key for Server
+  # # Bouncing = 1
+  # # NAT-PMP (Port Forwarding) = off
+  # # VPN Accelerator = on
+  # PrivateKey = mFkBhnjSlGoqGMVQ+r+ur51aKVQ8/wYjpymPtRYPAEE=
+  # Address = 10.2.0.2/32
+  # DNS = 10.2.0.1
+
+  # [Peer]
+  # # NL-FREE#345073
+  # PublicKey = +kfPCjoNEateo3jVc9tcduKh6nwQpoKx0/JXxgjHD2c=
+  # AllowedIPs = 0.0.0.0/0
+  # Endpoint = 190.2.147.7:51820
+
   # "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix" creates a
   # disk with this label on first boot. Therefore, we need to keep it. It is the
   # only information from the installer image that we need to keep persistent
