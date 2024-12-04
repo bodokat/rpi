b530924646cda2b21eb2f25aa4fb73641914836e
berni <bod.kato@gmail.com>
2024-12-04 22:07:39 +0100

    dashy

--- media.nix
+++ media.nix
@@ -7,7 +7,7 @@
 }:
 {
 
-  users.groups."media" = {};
+  users.groups."media" = { };
 
   users.users."media" = {
     group = "media";
@@ -20,60 +20,123 @@
     };
   };
 
-
   services.qbittorrent = {
-      enable = true;
-      openFirewall = true;
-      i2p = {
-        port = 8080;
-        user = "media";
+    enable = true;
+    openFirewall = true;
+    i2p = {
+      port = 8080;
+      user = "media";
+    };
+    vpn = {
+      port = 8090;
+      user = "media";
+    };
+  };
+  systemd.services.qbittorrent-vpn.vpnConfinement = {
+    enable = true;
+    vpnNamespace = "wg";
+  };
+
+  services.prowlarr = {
+    enable = true;
+    openFirewall = true;
+  };
+
+  services.radarr = {
+    enable = true;
+    openFirewall = true;
+    user = "media";
+    group = "media";
+  };
+
+  services.sonarr = {
+    enable = true;
+    openFirewall = true;
+    user = "media";
+    group = "media";
+  };
+
+  services.jellyfin = {
+    enable = true;
+    openFirewall = true;
+    user = "media";
+    group = "media";
+  };
+
+  services.jellyseerr = {
+    enable = true;
+    openFirewall = true;
+  };
+
+  services.dashy = {
+    enable = true;
+    settings = {
+      pageInfo = {
+        title = "Amogus";
+        description = "ඞඞඞඞඞඞඞඞඞඞඞඞඞ";
       };
-      vpn = {
-        port = 8090;
-        user = "media";
-      };
+      sections = [
+        {
+          name = "lol";
+          items = [
+            {
+              title = "qbittorrent (vpn)";
+              icon = "si-qbittorrent";
+              url = "berni-pi:${toString config.services.qbittorrent.vpn.port}";
+            }
+            {
+              title = "qbittorrent (i2p)";
+              icon = "si-qbittorrent";
+              url = "berni-pi:${toString config.services.qbittorrent.i2p.port}";
+            }
+            {
+              title = "jellyfin";
+              icon = "si-qbittorrent";
+              url = "berni-pi:8096";
+            }
+            {
+              title = "jellyseerr";
+              icon = "si-qbittorrent";
+              url = "berni-pi:${toString config.services.jellyseerr.port}";
+            }
+            {
+              title = "i2pd";
+              icon = "hl-i2p-light";
+              url = "berni-pi:${config.services.i2pd.proto.http.port}";
+            }
+            {
+              title = "radarr";
+              icon = "si-radarr";
+              url = "berni-pi:7878";
+            }
+            {
+              title = "sonarr";
+              icon = "si-sonarr";
+              url = "berni-pi:8989";
+            }
+            {
+              title = "prowlarr";
+              icon = "si-prowlarr";
+              url = "berni-pi:9696";
+            }
+          ];
+        }
+      ];
     };
-    systemd.services.qbittorrent-vpn.vpnConfinement = {
-      enable = true;
-      vpnNamespace = "wg";
-    };
+  };
 
-    services.prowlarr = {
-      enable = true;
-      openFirewall = true;
-    };
-
-    services.radarr = {
-      enable = true;
-      openFirewall = true;
-      user = "media";
-      group = "media";
-    };
-
-    services.sonarr = {
-      enable = true;
-      openFirewall= true;
-      user = "media";
-      group = "media";
-    };
-
-
-    services.jellyfin = {
-      enable = true;
-      openFirewall = true;
-      user = "media";
-      group = "media";
-    };
-
-    services.jellyseerr = {
-      enable = true;
-      openFirewall = true;
-    };
+  services.caddy = {
+    enable = true;
+    virtualHosts."berni-pi.tailb15778.ts.net".extraConfig = ''
+      root ${config.services.dashy.finalDrv}
+      try_files {path} /index.html
+    '';
+  };
 
   age.secrets."protonvpn.conf".file = ./secrets/protonvpn.conf.age;
   age.secrets."protonvpn_key".file = ./secrets/protonvpn_key.age;
 
-  networking.firewall.allowedUDPPorts = [51820];
+  networking.firewall.allowedUDPPorts = [ 51820 ];
 
   vpnNamespaces.wg = {
     enable = true;
@@ -83,11 +146,16 @@
       "127.0.0.0/24"
     ];
     portMappings =
-    let mkPort = p: {from = p; to = p;}; in
-    [
-      (mkPort config.services.transmission.settings.rpc-port)
-      (mkPort config.services.qbittorrent.vpn.port)
-    ];
+      let
+        mkPort = p: {
+          from = p;
+          to = p;
+        };
+      in
+      [
+        (mkPort config.services.transmission.settings.rpc-port)
+        (mkPort config.services.qbittorrent.vpn.port)
+      ];
 
     openVPNPorts = [
       {
@@ -105,15 +173,12 @@
     ];
   };
 
-
-
   # systemd.services.transmission = {
   #   vpnConfinement = {
   #     enable = true;
   #     vpnNamespace = "wg";
   #   };
   # };
-
 
   # services.transmission = {
   #   enable = true;
---
+++ network.nix
@@ -0,0 +1,41 @@
+{
+  pkgs,
+  config,
+  lib,
+  inputs,
+  ...
+}:
+{
+  services.blocky = {
+    enable = true;
+    settings = {
+      ports.dns = 53; # Port for incoming DNS Queries.
+      upstreams.groups.default = [
+        "https://one.one.one.one/dns-query" # Using Cloudflare's DNS over HTTPS server for resolving queries.
+      ];
+      # For initially solving DoH/DoT Requests when no system Resolver is available.
+      bootstrapDns = {
+        upstream = "https://one.one.one.one/dns-query";
+        ips = [
+          "1.1.1.1"
+          "1.0.0.1"
+        ];
+      };
+
+      customDns = {
+        mapping = {
+          "berni.sus" = "100.122.84.31";
+        };
+      };
+    };
+  };
+
+  services.caddy = {
+    enable = true;
+    virtualHosts = {
+      "test.berni.sus".extraConfig = ''
+        respond "Hi :3"
+      '';
+  };
+
+}
